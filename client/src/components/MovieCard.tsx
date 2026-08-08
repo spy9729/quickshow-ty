95e2e25c42f9a36ee2b1e782cf8e492ddd291974
spy9729 <spy9729@gmail.com>
2026-08-08 16:20:39 +0530

    final commit before debbing

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -11,6 +11,8 @@ interface MovieCardProps {
 const MovieCard = ({ movie }: MovieCardProps) => {
   const { image_base_url } = useAppContext();
   const navigate = useNavigate();
+
+  if (!movie) return null;
   return (
     <div className="flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:-translate-y-1 transition duration-300 w-62">
       <img
