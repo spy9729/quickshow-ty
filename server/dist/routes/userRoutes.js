a6958e83c390a5b94bc95b2a3a7c4388bcbf52bf
spy9729 <spy9729@gmail.com>
2026-08-11 01:31:54 +0530

    build

--- server/dist/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getFavorites, getUserBookings, updateFavorite, } from "../controllers/userController.js";
-const userRouter = express.Router();
-userRouter.get("/bookings", getUserBookings);
-userRouter.post("/update-favorite", updateFavorite);
-userRouter.get("/favorites", getFavorites);
-export default userRouter;
